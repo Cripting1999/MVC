a9b04f2199d4b639b5ea2b1cc57e89ff4b6d18d3
Cripting1999 <s17120252@alumnos.itsur.edu.mx>
2021-05-29 15:37:09 -0500

    Enrutamiento API REST P1

--- Startup.cs
+++ Startup.cs
@@ -25,51 +25,28 @@ namespace MvcMovie
          public IWebHostEnvironment Environment { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllersWithViews();
+       public void ConfigureServices(IServiceCollection services)
+{
+    services.AddControllers();
+}
 
-            services.AddDbContext<MvcMovieContext>(options =>
-            {
-                var connectionString = Configuration.GetConnectionString("MvcMovieContext");
+public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+{
+    if (env.IsDevelopment())
+    {
+        app.UseDeveloperExceptionPage();
+    }
 
-                if (Environment.IsDevelopment())
-                {
-                    options.UseSqlite(connectionString);
-                }
-                else
-                {
-                    options.UseSqlServer(connectionString);
-                }
-            });
-        }
+    app.UseHttpsRedirection();
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
+    app.UseRouting();
 
-            app.UseRouting();
+    app.UseAuthorization();
 
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-            });
-        }
+    app.UseEndpoints(endpoints =>
+    {
+        endpoints.MapControllers();
+    });
+}
     }
 }
